d27004cdc30e146b16ba4e58f799cd582946b779
Poppy507 <101574826+Poppy507@users.noreply.github.com>
2023-05-05 09:26:58 +0100

    Add files via upload

---
+++ 1Hz_bwod.ino
@@ -0,0 +1,34 @@
+
+const int ledPin =  LED_BUILTIN;
+const int pulse = 2;
+
+int ledState = LOW;
+
+unsigned long previousMillis = 0;
+
+// constants won't change:
+const long interval = 1000;           // blinking time period (milliseconds)
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(pulse, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    digitalWrite(ledPin, ledState);
+    digitalWrite(pulse, ledState);
+  }
+}
